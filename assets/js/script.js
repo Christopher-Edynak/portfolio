388726d72bff1740e7fcae1e46addef14f991716
[Christopher Edynak] <[christopher.edynak@outlook.com]>
2022-09-12 16:42:06 -0400

    implemented owl carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
     strings: ["Software Engineer", "Web Developer", "Mobile/App Developer", "Information Technology Professional", "Security Specialist", "Networking Professional", "Private Cloud Architect"], typeSpeed: 70, loop: true, startDelay: 1000, showCursor: false 
   });
 
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  })
+
 });
