fac0d146ce468d1bd1235efd070a0dc4d4269b3e
[Christopher Edynak] <[christopher.edynak@outlook.com]>
2022-09-12 16:20:22 -0400

    added text to about section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   var typed = new Typed(".typed", {
-    strings: ["Software Engineer", "Web Developer", "Information Technology Professional", "Security Specialist", "Networking Professional", "Private Cloud Architect"], typeSpeed: 70, loop: true, startDelay: 1000, showCursor: false 
+    strings: ["Software Engineer", "Web Developer", "Mobile/App Developer", "Information Technology Professional", "Security Specialist", "Networking Professional", "Private Cloud Architect"], typeSpeed: 70, loop: true, startDelay: 1000, showCursor: false 
   });
 
 });
