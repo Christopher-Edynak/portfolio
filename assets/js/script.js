fc14f990d4e569e4cb5948d4c1a6c54f257b96ba
[Christopher Edynak] <[christopher.edynak@outlook.com]>
2022-09-12 17:09:42 -0400

    implemented pie chart options

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,34 @@ $(document).ready(function(){
 
   $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
-    nav:true,
+    items: 4,
     responsive:{
         0:{
             items:1
         },
-        600:{
+        480:{
+            items:2
+        },
+        768:{
             items:3
         },
-        1000:{
-            items:5
-        }
+        938:{
+          items:4
+      }
     }
   });
 
   $('.chart').easyPieChart({
     //your options goes here
+    easing: 'easeInOut',
+    barColor: '#FF0000',
+    trackColor: '#000',
+    scaleColor: false,
+    lineWidth: 4,
+    size: 152,
+    onstep: function(from, to, percent) {
+      $(this.el).find('.percent').text(Math.round(percent));
+    }
   });
 
 });
